2c155edaf349a7de01c17b82e5b9c563320863d0
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-08-22 16:38:04 -0400

    changes

--- allears_backend/allears_backend/settings.py
+++ allears_backend/allears_backend/settings.py
@@ -43,7 +43,6 @@ INSTALLED_APPS = [
 
     # Local apps
     'allears',
-    'snippets',
     'answers',
     'questions',
     'users',
--- allears_backend/answers/views.py
+++ allears_backend/answers/views.py
@@ -1,10 +1,3 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from .models import QuestionsModel, AnswersModel
-
-class QuestionsModelViewSet(viewsets.ModelViewSet):
-    queryset = QuestionsModel.objects.all()
-    serializer_class = QuestionsModelSerializer
-class AnswersModelViewSet(viewsets.ModelViewSet):
-    queryset = AnswersModel.objects.all()
-    serializer_class = AnswersModelSerializer
