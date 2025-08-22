af6704b6879f973c626b1b0d912eac9b06390bc6
zkp117 <160277165+zkp117@users.noreply.github.com>
2025-08-22 17:43:42 -0400

    restarting

--- allears_backend/users/views.py
+++ allears_backend/users/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Question
+from .serializers import QuestionSerializer
 
-# Create your views here.
+@api_view(['GET', 'POST'])
+def question_list(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        serializer = QuestionSerializer(questions, many=True)
+        return Response(serializer.data)
+
+    elif request.method == 'POST':
+        serializer = QuestionSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class UserInbox:
+    def __init__(self, user):
+        self.user = user
+
+    def get_questions(self):
+        return f"Your unanswered questions: {self.user.questions.filter(answered=False)}"
+    
+    
